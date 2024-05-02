70cd936336ce32f0f32c32e4cea1a78d840ecfd0
Vincent Bollot <bollot.vincent@gmail.com>
2024-05-02 10:45:37 +0200

    sql working

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def ord(char: str) -> int:
-    
+# generer un sel aleatoire
+
+# retoune le code ASCII d'un caractere passe en parametre
+print(ord('z'))
+number_ascii = ord('z')
+char_ascii = chr(number_ascii)
+print(char_ascii)
+
+def cle(t: tuple) -> str:
+    res = ''
+    for i in range(len(t)):
+        res += chr(t[i])
+    return res
