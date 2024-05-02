fb8c8b20b3f58a94dd75102a1919df17ef2f877e
Vincent Bollot <bollot.vincent@gmail.com>
2024-05-02 16:41:00 +0200

    utils de chiffrement

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# # generer un sel aleatoire
 
-# # retoune le code ASCII d'un caractere passe en parametre
-# print(ord('z'))
-# number_ascii = ord('z')
-# char_ascii = chr(number_ascii)
-# print(char_ascii)
-
-# def cle(t: tuple) -> str:
-#     res = ''
-#     for i in range(len(t)):
-#         res += chr(t[i])
-#     return res
 
 from src.dao.utilisateur_dao import UtilisateurDao
 from src.models.utilisateur import Utilisateur
